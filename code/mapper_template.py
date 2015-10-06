1e37c88b3dd115244a2a77056a633dc24a9af1bb
taivop <taivo.pungas@gmail.com>
2015-10-06 11:07:41 +0200

    add hash and emit functions to mapper

--- code/mapper_template.py
+++ code/mapper_template.py
@@ -4,6 +4,14 @@
 import numpy as np
 import sys
 
+def hash(s, a, b):
+    return np.dot(s, a) + b % 9907
+
+# l is a list, will be emitted with tab separation
+def emit(ls):
+    ls = map(lambda l: str(l), ls)
+    print('\t'.join(ls))
+
 if __name__ == "__main__":
     # VERY IMPORTANT:
     # Make sure that each machine is using the
@@ -15,4 +23,8 @@ if __name__ == "__main__":
         line = line.strip()
         video_id = int(line[6:15])
         shingles = np.fromstring(line[16:], sep=" ")
-        
+
+
+
+
+
