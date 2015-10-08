9d86db08f2026c283b8074b2eb8b53d9e512f3e9
taivop <taivo.pungas@gmail.com>
2015-10-08 13:12:01 +0200

    fix argument order

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 cat data/training.txt|python2.7 code/mapper.py|sort|python2.7 code/reducer.py > data/duplicates_found.txt
 
 # Check our duplicates against provided ground truth
-python2.7 code/check.py data/duplicates.txt data/duplicates_found.txt
+python2.7 code/check.py data/duplicates_found.txt data/duplicates.txt
