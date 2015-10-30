ca0273f8ef56a1861bf6b379cf0df7a5dc3bbac2
taivop <taivo.pungas@gmail.com>
2015-10-30 09:48:55 +0100

    clean up mapper

--- project2/code/mapper.py
+++ project2/code/mapper.py
@@ -17,10 +17,7 @@ TRANS_DIM = 400
 
 def transform(x_original):
     np.random.seed(42)
-    #return x_original
-    #trans_mat = np.random.randn(DIMENSION)
-    #trans_mat = np.dot(trans_mat,trans_mat)
-    #np.transpose(x_original)
+
     temp = 2 * np.pi * np.random.randn(DIMENSION, TRANS_DIM)
     temp_omiga = np.dot(x_original, temp)
     b = 2 * np.pi * np.random.rand(1, TRANS_DIM)
@@ -35,33 +32,12 @@ def emit(weights):
         print(str(w) + "\t"),
 
     print("")
-#def cross_validation(batch, labels):
 
 
 
 def process_batch(batch, labels):
     """Process a batch of examples: calculate and emit the corresponding weight vector.
     Each row in matrix 'batch' holds an example. Each element in vector 'labels' holds the corresponding label."""
-
-    #print("MAPPER: Processing batch of shape %dx%d", batch.shape)
-    # model = SVC(kernel="linear")
-    # model.fit(batch, labels)
-    # weights = model.coef_
-    # weights.shape = DIMENSION
-    # emit(weights)
-
-    # weights = np.zeros(shape=TRANS_DIM)  # TODO calculate weight vector on this batch
-    #
-    # for ii in range(batch.shape[0]):
-    #     YETA = 1 / (np.sqrt(ii+1))
-    #     trans_batch = (batch[ii, :])
-    #
-    #     if (labels[ii]*(np.dot(weights, trans_batch))) < 1:
-    #         weights += YETA*labels[ii]*trans_batch
-    #
-    #     weights = np.dot(np.min([1, 1 / (np.sqrt(LAMDA) * np.sqrt(np.dot(weights, weights)))]), weights)
-    #
-    #     emit(weights / batch.shape[0])
 
     model = LinearSVC(fit_intercept=False)
     model.fit(batch, labels)
@@ -79,15 +55,12 @@ if __name__ == "__main__":
         line = line.strip()
         (label, x_string) = line.split(" ", 1)
         label = int(label)
-        # x_original = np.fromstring(x_string, sep=' ')
         x_original = np.fromstring(x_string, sep=' ')
-        #x.shape = (1, DIMENSION)    # Force x to be a row vector
         x_original.shape = (1, DIMENSION)
         x = transform(x_original)   # Use our features.
         # Add row to batch
         batch = np.concatenate((batch, x))
         labels.append(label)
-        #print(label)
 
         # If batch has BATCH_SIZE examples then calculate weight vector on that batch, process batch and start a new one
         if batch.shape[0] == BATCH_SIZE:
