e0d2cc18e257e033b41e4818b9fa4484a0003568
taivop <taivo.pungas@gmail.com>
2015-10-30 13:07:16 +0100

    add time tracking to evaluation scripts

--- project2/evaluate.sh
+++ project2/evaluate.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-sh run_large.sh > weights/weights_evaluate.txt
-mkdir test_data
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo 'Evaluating on LARGE dataset...'
+start=`date +%s`
+sh run.sh > weights/weights_evaluate.txt
+end=`date +%s`
 cat data/training.txt | cut -d ' ' -f1 > test_data/test_labels.txt
 cat data/training.txt | cut -d ' ' -f 2- > test_data/test_features.txt
-echo "Accuracy:"
+printf "Accuracy: ${RED}"
 python2.7 code/evaluate.py weights/weights_evaluate.txt test_data/test_features.txt test_data/test_labels.txt code
+
+runtime=$((end-start))
+printf "${NC}Training time: ${RED}${runtime}${NC} seconds\n"
--- project2/evaluate_small.sh
+++ project2/evaluate_small.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo 'Evaluating on SMALL dataset...'
+start=`date +%s`
 sh run.sh > weights/weights_evaluate_small.txt
+end=`date +%s`
 cat data/training_small.txt | cut -d ' ' -f1 > test_data/test_labels_small.txt
 cat data/training_small.txt | cut -d ' ' -f 2- > test_data/test_features_small.txt
-echo "Accuracy:"
+printf "Accuracy: ${RED}"
 python2.7 code/evaluate.py weights/weights_evaluate_small.txt test_data/test_features_small.txt test_data/test_labels_small.txt code
+
+runtime=$((end-start))
+printf "${NC}Training time: ${RED}${runtime}${NC} seconds\n"
