4b00ffbb25919f88c31ab6ed929859419cbca061
taivop <taivo.pungas@gmail.com>
2015-12-11 11:03:33 +0100

    optimise: invert M much less often

--- project4/code/policy.py
+++ project4/code/policy.py
@@ -9,6 +9,7 @@ DIMENSION = 6
 
 
 M = dict()  # Key: article ID. Value: matrix M for LinUCB algorithm.
+M_inv = dict() # Key: article ID. Value: inverted matrix M.
 b = dict()  # Key: article ID. Value: number b for LinUCB algorithm.
 w = dict()  # Key: article ID. Value: weights w for LinUCB algorithm.
 # ucb_value = dict()  # Key: article id. Value: ucb value for LinUCB algorithm.
@@ -32,6 +33,7 @@ def set_articles(articles):
     for article_id in article_list:
         # Initialise M and b
         M[article_id] = np.identity(DIMENSION)
+        M_inv[article_id] = np.identity(DIMENSION)
         b[article_id] = np.zeros((DIMENSION, 1))
         w[article_id] = np.linalg.inv(M[article_id]).dot(b[article_id])
 
@@ -44,6 +46,7 @@ def update(reward):
     else:
         # Update M, b and weights
         M[last_article_id] += last_user_features.dot(last_user_features.T)
+        M_inv[last_article_id] = np.linalg.inv(M[last_article_id])
         b[last_article_id] += reward * last_user_features
         w[last_article_id] = np.linalg.inv(M[last_article_id]).dot(b[last_article_id])    # Update weight
 
@@ -61,6 +64,7 @@ def reccomend(time, user_features, articles):
         if article_id not in M:
             # Initialise this article's variables
             M[article_id] = np.identity(DIMENSION)
+            M_inv[article_id] = np.identity(DIMENSION)
             b[article_id] = np.zeros((DIMENSION, 1))
             w[article_id] = M[article_id].dot(b[article_id])    # Don't need to invert M because it's an identity matrix
 
@@ -71,7 +75,7 @@ def reccomend(time, user_features, articles):
         # If we have seen article before
         else:
             ucb_value = w[article_id].T.dot(user_features) +\
-                        ALPHA * np.sqrt(user_features.T.dot(np.linalg.inv(M[article_id])).dot(user_features))
+                        ALPHA * np.sqrt(user_features.T.dot(M_inv[article_id]).dot(user_features))
 
             if ucb_value > best_ucb_value:
                 best_ucb_value = ucb_value
